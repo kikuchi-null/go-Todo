4b166e0957c82e7e9168fb3d9bf2689ab80ca229
Taiki Kikuchi <taikikikuchi@Taikis-MacBook-Air.local>
2021-07-04 22:41:48 +0900

    first commit

--- app/main.go
+++ app/main.go
@@ -6,11 +6,4 @@ func main() {
 
 	controllers.StartServer()
 
-	/*
-		//user, _ := models.GetUserByID(1)
-		todo, _ := models.GetTodo(1)
-		task := "Learning Golang and BeeGo"
-		//user.CreateTodo(task)
-		todo.UpdateTodo(task)
-	*/
 }
