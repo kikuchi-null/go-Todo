10a44c9a12bb2f6d2ff8e2e3e47989036dbb823e
Taiki Kikuchi <taikikikuchi@Taikis-MacBook-Air.local>
2021-09-29 18:24:39 +0900

    bug fixed

--- mysql/init/init.sql
+++ mysql/init/init.sql
@@ -2,19 +2,17 @@
 CREATE DATABASE IF NOT EXISTS todo;
 use todo;
 
-CREATE TABLE IF NOT EXISTS users
-(
+CREATE TABLE IF NOT EXISTS users(
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     uuid VARCHAR(36) NOT NULL UNIQUE, 
     name VARCHAR(225) NOT NULL,
     email VARCHAR(225) NOT NULL UNIQUE, 
     password VARCHAR(225) NOT NULL, 
     create_at DATE NOT NULL,
-    update_at TIMESTAMP NOT NULL, 
+    update_at TIMESTAMP NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS sessions
-(
+CREATE TABLE IF NOT EXISTS sessions(
     session_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     uuid VARCHAR(36) NOT NULL UNIQUE, 
@@ -23,13 +21,13 @@ CREATE TABLE IF NOT EXISTS sessions
     FOREIGN KEY(user_id) REFERENCES users(user_id)
 );
 
-CREATE TABLE IF NOT EXISTS todos
-(
+
+CREATE TABLE IF NOT EXISTS todos(
     todo_id INT AUTO_INCREMENT PRIMARY KEY, 
     user_id INT NOT NULL, 
     content VARCHAR(255) NOT NULL, 
     create_at DATE NOT NULL, 
     update_at TIMESTAMP NOT NULL,
-    deadline DATETIME NOT NULL
+    deadline DATETIME NOT NULL,
     FOREIGN KEY(user_id) REFERENCES users(user_id)
 );
