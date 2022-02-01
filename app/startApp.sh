9b1a29295d5d2cc805bc96f2302f2c0ef93751b1
Taiki Kikuchi <taikikikuchi@Taikis-MacBook-Air.local>
2022-02-01 21:09:52 +0900

    fix shellscript

--- app/startApp.sh
+++ app/startApp.sh
@@ -1,12 +1,12 @@
 #!/bin/ash
 
-echo "==>Checking..."
+echo "==> Checking..."
 go mod tidy >/dev/null 2>&1
 
 if [ $? -eq 0 ]; then
-    echo "==>Go live (http://localhost:8080/)"
+    echo "==> Go live (http://localhost:8080/)"
     go run main.go
 else
-    echo "==>Failed to start"
+    echo "==> Failed to start"
 fi
 
